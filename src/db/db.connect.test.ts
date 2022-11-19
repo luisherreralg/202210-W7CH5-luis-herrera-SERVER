c84aebe5c1132e75c991319a5ffcea75dda660d3
luisherreralg <lherreralg@outlook.es>
2022-11-19 10:50:07 +0100

    ✅ Added db.connect tests

---
+++ src/db/db.connect.test.ts
@@ -0,0 +1,12 @@
+import { dbConnect } from './db.connect.js';
+import mongoose from 'mongoose';
+
+describe('Given the db.connect module', () => {
+    describe('When we use it', () => {
+        test('Then it should return a type of mongoose', async () => {
+            const result = await dbConnect();
+            expect(typeof result).toBe(typeof mongoose);
+            mongoose.disconnect();
+        });
+    });
+});
