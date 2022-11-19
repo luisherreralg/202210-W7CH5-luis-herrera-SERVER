6fcfead714eb49824ae75121089700c65b27a846
luisherreralg <lherreralg@outlook.es>
2022-11-19 10:53:01 +0100

    ✅ Added auth tests

---
+++ src/tools/auth.test.ts
@@ -0,0 +1,38 @@
+import { createToken, readToken } from './auth';
+import jwt from 'jsonwebtoken';
+
+describe('Given createToken function', () => {
+    describe('When its invoked', () => {
+        test('Then it should return...', () => {
+            const signSpy = jest.spyOn(jwt, 'sign');
+            const mockPayload = { userName: 'pepe' };
+            const result = createToken(mockPayload);
+            expect(typeof result).toBe('string');
+            expect(signSpy).toHaveBeenCalled();
+        });
+    });
+});
+
+describe('Given the readToken function', () => {
+    const mockPayload = { userName: 'pepe' };
+    const token = createToken(mockPayload);
+
+    describe('When its invoked with a valid token', () => {
+        test('Then it should return the payload', () => {
+            const result = readToken(token);
+            console.log(token);
+            expect(result.userName).toEqual(mockPayload.userName);
+        });
+
+        test('Not vaild test', () => {
+            const invalidToken =
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InBlcGUiLCJpYXQiOjE2Njg3NzM1Nzl9.Fc-onucI_EXD53y72Qoj5MEGlu3VlP4l0CApxF2n0RA';
+
+            expect(() => readToken(invalidToken)).toThrow();
+
+            // Esto no funciona, ya que se lanza fuera el error fuera del expect
+            // const result = readToken(invalidToken);
+            // expect(() => result).toThrow();
+        });
+    });
+});
