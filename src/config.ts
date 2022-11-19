3ec4a1be76a343cfa22c4595e44827a382bbd3d3
luisherreralg <lherreralg@outlook.es>
2022-11-19 16:52:41 +0100

    🔧 Updated config file

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+export const PORT = process.env.PORT;
 export const USER = process.env.USER;
 export const PW = process.env.PW;
 export const CLUSTER = process.env.CLUSTER;
+export const URL = process.env.URL;
+export const SECRET = process.env.SECRET;
